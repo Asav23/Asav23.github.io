e799789b17fee9169d542490d30001635c5be74d
Asav23 <160866712+Asav23@users.noreply.github.com>
2025-03-01 15:08:09 +0000

    Initialised

---
+++ script.js
@@ -0,0 +1,4 @@
+// Optional JavaScript to log when the link is clicked or perform additional actions
+document.querySelector('.chatbot-link').addEventListener('click', function() {
+    console.log('Chatbot link clicked!');
+});
